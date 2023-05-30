3427b0db835682859e77aadbc752eae64552f216
Alex Torres <alextorressk@gmail.com>
2023-05-30 13:17:07 -0500

    Initial commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
+import { ThemeProvider } from "@material-tailwind/react";
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
